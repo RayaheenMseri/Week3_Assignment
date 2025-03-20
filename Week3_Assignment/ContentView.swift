e4aec0dbb6767e2caecbb6b3e2f7937d378b4db9
RayaheenMseri <rayaheenmseri@gmail.com>
2025-03-21 00:15:38 +0300

    Initial Commit

---
+++ Week3_Assignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Week3_Assignment
+//
+//  Created by Rayaheen Mseri on 21/09/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Week3_Assignment/Week3_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week3_AssignmentApp.swift
+//  Week3_Assignment
+//
+//  Created by Rayaheen Mseri on 21/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Week3_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
